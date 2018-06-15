1e4b50f02a3c2069262204fce8f6db09de00ca9b
Valentin BELAUD <valentin@noosys.fr>
2018-06-15 16:12:48 +0200

    Send notification to all users.

--- api/sockets/notificationSocket.js
+++ api/sockets/notificationSocket.js
@@ -11,11 +11,22 @@ var Users = mongoose.model('Users');
  * @param {Notification} notification
  * @author Martin Allimonier <martin@noosys.fr>
  */
-module.exports.sendNotificationToUser = function(socketId, notification, callback){
+module.exports.sendNotificationToUser = function (socketId, notification, callback) {
     var socket = io.sockets.sockets[socketId];
-    if(socket){
+    if (socket) {
         socket.emit('new_notification', notification, (data) => {
             callback(data);
         });
     }
+}
+
+/**
+ * @description function that send a notification to all users
+ * @param {String} notification 
+ * @param {return} callback 
+ */
+module.exports.sendNotificationToAll = function (notification, callback) {
+    socket.broadcast.emit('all_users', notification, (data) => {
+        callback(data);
+    })
 }
