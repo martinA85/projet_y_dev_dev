a43fc059ba1617220a26696d83e0a9782fedc078
Martin Allimonier <allimonier.martin@gmail.com>
2018-06-27 15:23:05 +0200

    adding docs comment to category controller

--- api/controllers/categoryController.js
+++ api/controllers/categoryController.js
@@ -1,3 +1,19 @@
+//Doc snippet
+
+/**
+ * @apiDefine CategObject
+ * @apiSuccessExample Success-response
+ * Example of categ object
+ * {
+ *      name : "Example Categ"
+ * }
+ */
+
+/**
+ * @apiDefine paramCategId
+ * @apiParam {String} :categoryId Categ unique id (_id)
+ */
+
 'use strict';
 
 var mongoose =require("mongoose");
@@ -7,7 +23,19 @@ var Category = mongoose.model("Category");
 // ======================  CRUD  ======================
 // ====================================================
 
-//return the new category object or error
+/**
+ * @api {POST} /category Creating new category
+ * @apiName CreateCategory
+ * @apiGroup Category
+ * @apiParam {Category} Category Category object to create
+ * @apiParamExample CategoryObject
+ * {
+ *      name : "Example Categ"
+ * }
+ * 
+ * @apiSuccess {Category} CategoryObject Category object after his creation
+ * @apiUse CategObject
+ */
 exports.createCategory = function(request, response){
     let new_category = new Category(request.body);
     new_category.save(function(err, new_category){
@@ -18,7 +46,18 @@ exports.createCategory = function(request, response){
     });
 }
 
-//return all the categories or error
+/**
+ * @api {GET} /category Getting All Category
+ * @apiName GetAllCategory
+ * @apiGroup Category
+ * @apiSuccess {ObjectList} CategList 
+ * @apiSuccessExample Success-response
+ * [
+ *      {
+ *          name:"Categ name"
+ *      }
+ * ]
+ */
 exports.getAllCategory = function(request, response){
     Category.find({}, function(err, categories){
         if(err){
@@ -28,7 +67,13 @@ exports.getAllCategory = function(request, response){
     })
 }
 
-//return a category by his id
+/**
+ * @api {GET} /category/:categoryId Getting one category
+ * @apiName GetOneCategory
+ * @apiGroup Category
+ * @apiUse CategObject
+ * @apiSuccess {Category} Category Category Object
+ */
 exports.getCategoryById = function(request, response){
     Category.findById(request.params.categoryId, function(err, category){
         if(err){
@@ -39,7 +84,13 @@ exports.getCategoryById = function(request, response){
 }
 
 
-//return a category after update
+/**
+ * @api {PUT} /category/:categoryId Updating a category
+ * @apiName UpdateGategory
+ * @apiGroup Category
+ * @apiUse CategObject
+ * @apiSuccess {Category} Category Object
+ */
 exports.updateCategory = function(request, response){
     Category.findOneAndUpdate({_id:request.params.categoryId}, request.body, {new:true}, function(err, category){
         if(err){
@@ -49,7 +100,19 @@ exports.updateCategory = function(request, response){
     });
 }
 
-//return message or error
+/**
+ * @api {DELETE} /category/:categoryId Deleting a category
+ * @apiName DeleteGategory
+ * @apiGroup Category
+ * @apiUse CategObject
+ * @apiSuccess {Boolean} Success Success State
+ * @apiSuccess {String} message Message returned
+ * @apiSuccessExample Success-Response
+ * {
+ *      success : true,
+ *      message : "Category deleted"
+ * }
+ */
 exports.deleteCategory = function(request, response){
     Category.remove({_id:request.params.categoryId}, function(err, event){
         if(err){
